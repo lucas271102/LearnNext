f3b107168fe3a0d21558bd8caea3bc7330005cb3
lucas271102 <lucas001spe@gmail.com>
2024-06-03 14:49:42 -0300

    error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,7 +13,7 @@ const FormSchema = z.object({
   });
    
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
-   
+  const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
 
@@ -26,11 +26,48 @@ export async function createInvoice(formData: FormData) {
    })
    const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0];
-
-   await sql`
-   INSERT INTO invoices (customer_id, amount, status, date)
-   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
- `;
+try {
+    
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+} catch (error) {
+    return {message:'Failed to create invoice'}
+}
 revalidatePath('/dashboard/invoices')
 redirect('/dashboard/invoices')
 }
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   try {
+       await sql`
+         UPDATE invoices
+         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+         WHERE id = ${id}
+       `;
+      
+    } catch (error) {
+        return{message:'Failed to update invoice '}
+    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+ 
+  }
+  export async function deleteInvoice(id: string) {
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+        revalidatePath('/dashboard/invoices');
+        return {message:'Successfully deleted'}
+    } catch (error) {
+        return{message: 'Failed to delete invoice'}
+    }
+  }
