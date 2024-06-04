c5a8f4cf37bfe63af6fddfab69600f3326bac744
lucas271102 <lucas001spe@gmail.com>
2024-06-04 16:54:42 -0300

    SEO

--- app/dashboard/customers/page.jsx
+++ app/dashboard/customers/page.jsx
@@ -1,3 +1,10 @@
+import { Metadata as metadata } from "next"
+export const metadata = {
+    title: 'Customers',
+  };
+  
+
+
 export default function Page(){
     return(
         <p>Customers Page</p>
